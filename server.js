96dd4f8fdd5c38061836a8e99ae0e9097744fe5f
mirkan1 <mirkanbaba1@gmail.com>
2020-12-10 20:30:50 -0500

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const port = 3000;
-var path = require('path');
+const PORT = process.env.PORT || 5000;
+const path = require('path');
 
+app
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/example.html'));
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});

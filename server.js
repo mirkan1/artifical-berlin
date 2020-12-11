5abc7fefb0e6ea420d81a4c3e28ffe9f61c8efb2
mirkan1 <mirkanbaba1@gmail.com>
2020-12-10 20:31:42 -0500

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const path = require('path');
 
 app
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
-	
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/example.html'));
 });

7efd85fd33aba362bfbe63c643fd7cc33563fb32
mirkan1 <mirkanbaba1@gmail.com>
2020-12-13 16:00:38 -0500

    more examples

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.get('/custom_object', (req, res) => {
   res.sendFile(path.join(__dirname + '/custom_object.html'));
 });
 
+app.get('/ply', (req, res) => {
+  res.sendFile(path.join(__dirname + '/ply.html'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
